ab140578f2285ae12e20e55f46ecbccf1a75a27d
Leo <leodahal4@gmail.com>
2019-12-23 16:39:16 +0545

    Deleted unnecessary Files

--- Files/try2.py
+++
@@ -1,8 +0,0 @@
-from os import path
-import subprocess
-
-if not(path.exists("/usr/local/lib/python2.7/dist-packages/notify2.py")): #or  path.exists("/usr/local/lib/python3.7/dist-packages/notify2.py")):
-    print("Installing.. ")
-    subprocess.call("pip install notify2",shell=True)    
-else:
-    print("Fuck u")
